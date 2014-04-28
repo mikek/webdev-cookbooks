e54463d7a582564740c07e6641eb636c41997662
Mikhail Kolesnik <mike@openbunker.org>
2014-04-28 15:48:50 +0400

    Disable upload_progress Nginx module config

--- nginx_configs/recipes/default.rb
+++ nginx_configs/recipes/default.rb
@@ -1,9 +1,9 @@
-cookbook_file "#{node['nginx']['dir']}/conf.d/upload_progress.conf" do
-  source "upload_progress.conf"
-  mode "0644"
-  owner "root"
-  group "root"
-end
+#cookbook_file "#{node['nginx']['dir']}/conf.d/upload_progress.conf" do
+#  source "upload_progress.conf"
+#  mode "0644"
+#  owner "root"
+#  group "root"
+#end
 
 cookbook_file "#{node['nginx']['dir']}/mime.types" do
   source "mime.types"
